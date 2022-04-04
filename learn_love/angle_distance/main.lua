faea5145ad80ec9b63188a83d248b76d0e24c2f9
Adi Permana <muh.adipermana@gmail.com>
2022-04-04 15:04:00 +0700

    Added mouse positioning using angle and the function math.atan2

---
+++ learn_love/angle_distance/main.lua
@@ -0,0 +1,34 @@
+function love.load()
+  -- Create obj circle
+  circle = {}
+
+  -- Circle props
+  circle.x = 100
+  circle.y = 100
+  circle.radius = 25
+  circle.speed = 200
+end
+
+function love.update(dt)
+  -- love.mouse.getPosition returns the x and y position of the cursor.
+  mouse_x, mouse_y = love.mouse.getPosition()
+
+  -- Get the angle i.e. vertical and horizontal vector (distance + direction), and returns an angle.
+  -- Arg 1 y-position [mouse_y] you want to go minus object [circle_y] y-position.
+  -- Arg 2 is the same with x position
+  angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
+end
+
+function love.draw()
+  love.graphics.circle("line", circle.x, circle.y, circle.radius)
+
+  -- prints the angle
+  love.graphics.print("Angle: " .. angle, 10, 10)
+
+  -- Some lines to visualize the velocity
+  love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
+  love.graphics.line(circle.x, circle.y, circle.x, mouse_y)
+
+  -- The angle
+  love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+end
