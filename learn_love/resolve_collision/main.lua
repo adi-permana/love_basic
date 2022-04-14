42efe084c5baee8fb87c58c84fbfb9cf53b17eb0
Adi Permana <muh.adipermana@gmail.com>
2022-04-14 16:05:39 +0700

    Fix #20: Added the abilty to push object, and also differences in strength between the objects and its effect on pushing ability

--- learn_love/resolve_collision/main.lua
+++ learn_love/resolve_collision/main.lua
@@ -6,20 +6,68 @@ function love.load()
   -- Then the entity classes
   require "player"
   require "wall"
+  require "box"
 
   -- Player first location
   player = Player(100, 100)
   -- Wall first location
   wall = Wall(200, 100)
+  wall2 = Wall(500, 100)
+  -- Box first location
+  box1 = Box(400, 150)
+  box2 = Box(420, 150)
+  box3 = Box(440, 150)
+ 
+  -- Objects table
+  objects = {}
+  table.insert(objects, player)
+  table.insert(objects, wall)
+  table.insert(objects, box1)
+  table.insert(objects, box2)
+  table.insert(objects, box3)
+  table.insert(objects, wall2)
 end
 
 function love.update(dt)
-  player:update(dt)
-  wall:update(dt)
-  wall:resolveCollison(player)
+  for i,v in ipairs(objects) do
+    -- Update the object
+    v:update(dt)
+  end
+
+  -- Keeps checking collision as long as there has been collision resolved
+  local loop = true
+  -- Limit loop so that it doesn't do it infinitely
+  local limit = 0
+
+  while loop do
+    -- First set it to false, if no collision it will stay false
+
+    -- Break loop if it reaches 100 cycles
+    limit = limit + 1
+    if limit > 100 then
+      -- When it still going above 100 loop; break loop
+      break
+    end
+
+  -- Go through the list of objs except the last one
+    for i=1,#objects-1 do
+      -- Then loop through the list, starting from the obj next to the object in the first loop [i + 1]
+      for j=i+1,#objects do
+        -- Resolve collision between item by going through next to each other; i.e. player:wall, player:box, wall,box;
+        -- While the last one doesn't
+        local collision = objects[i]:resolveCollison(objects[j])
+        -- Check collision if true run the loop again
+        if collision then
+          loop = true
+        end
+      end
+    end
+  end 
 end
 
 function love.draw()
-  player:draw()
-  wall:draw()
+  -- Draw each objects
+  for i,v in ipairs(objects) do
+    v:draw()
+  end
 end
